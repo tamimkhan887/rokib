66109196b54a57851c4a2a67b8cad564e8e344df
Tamim Khan <tamimkhan887@gmail.com>
2026-07-24 20:16:59 +0600

    Variable Add SuccessFull

---
+++ variable.js
@@ -0,0 +1,15 @@
+var weight = 38 ;
+var temperature = 38 ;
+// Variable declare 
+//  keyword variableName = value ;
+console.log(temperature)
+
+
+// let , const 
+
+let pricce = 100 ;
+price = 50 ;
+price = 200 ;
+
+
+const name = "Tamim Khan" ;
